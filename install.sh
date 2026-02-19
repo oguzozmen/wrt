8dfa89862bfe8f7cc0bb84116e5f7b0ea61e8c35
oguzozmen <oguzozmen38@gmail.com>
2026-02-19 12:30:18 +0300

    Simplify package installation in install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,13 @@
 #!/bin/sh
-PACKAGES="
-    luci-app-https-dns-proxy
-    luci-app-sqm
-    luci-app-nlbwmon
-    openssh-sftp-server
-    ca-certificates
-    fish
-    git
-    nano
-    gzip
-    zerotier
-    wget
-    unzip
-"
 opkg update 
-opkg install $PACKAGES 
+opkg install luci-app-sqm luci-app-nlbwmon openssh-sftp-server ca-certificates fish nano zerotier wget unzip
 
 wget --no-check-certificate -O /tmp/luci-app-cpu-status-mini_0.2.0-r1_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/24.10/luci-app-cpu-status-mini_0.2.0-r1_all.ipk
 opkg install /tmp/luci-app-cpu-status-mini_0.2.0-r1_all.ipk
 rm /tmp/luci-app-cpu-status-mini_0.2.0-r1_all.ipk
 service rpcd reload
 
+# luci-app-https-dns-proxy
 #--dpi-desync=fake --dpi-desync-ttl=3
+
 
